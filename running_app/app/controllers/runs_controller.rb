791514e4a3b53ecf670ac255dfe70abe170c9394
magser110 <124111918+magser110@users.noreply.github.com>
2025-09-16 15:30:08 -0500

    saving progress on backend calculations

--- running_app/app/controllers/runs_controller.rb
+++ running_app/app/controllers/runs_controller.rb
@@ -2,8 +2,13 @@ class RunsController < ApplicationController
   before_action :authenticate_request, except: [index]
 
   def index
-    runs = Run.all
-    render json: RunBlueprint.render(runs, view: :normal), status: :ok
+    runs = current_user.runs.order(created_at: :desc)
+    total_stats = calculate_total_stats(runs)
+
+    render json: {
+      RunBlueprint.render(runs, view: :normal),
+      total_stats: total_stats
+    }, status: :ok
   end
 
   def show
@@ -11,13 +16,13 @@ class RunsController < ApplicationController
   end
 
   def create 
-    run = Run.new(run_params)
+    run = current_user.runs.new(run_params)
 
     if run.save
       render json: RunBlueprint.render(run, view: :normal), status: :created
 
     else
-      render json: run.errors, status: unprocessable_entity
+      render json: {errores: run.errors.full_messages} , status: unprocessable_entity
     end
   end
 
@@ -39,11 +44,38 @@ class RunsController < ApplicationController
     end
   end
 
+  def stats 
+    runs = current_user.runs
+    total_stats = calculate_total_stats(runs)
+
+    render json: { 
+      total_stats: total_stats,
+       runs: runs.order(created_at: :desc)
+       }, status: :ok
+  end
+
   def set_run
     run = Run.find(params[:id])
   end
 
+  private
+
   def run_params
     params.permit(:distance, :time)
+  end
+
+  def calculate_total_stats(runs)
+    # total_distance = runs.sum(:distance)
+    # total_time = runs.sum(:time)
+
+    {
+      total_runs: runs.count,
+      total_distance: runs.sum(:distance).round(2), 
+      total_time: runs.sum(:time).round(2),
+      average_pace: runs.average(:pace).round(2),
+      avg_speed: runs.average(:average_speed).round(2),
+      total_calories: runs.sum(:calculated_calories),
+      total_steps: runs.sum(:calculated_steps)
+    }
   end
 end
--- running_app/app/controllers/users_controller.rb
+++ running_app/app/controllers/users_controller.rb
@@ -1,17 +1,32 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+
   def create 
     user = User.new(user_params)
     if user.save
-      render json: user, status: :created
+      token = JWT.encode({ user_id: user.id, exp: 24.hours.from_now.to_i }, Rails.application.credentials.secret_key_base)
+      response.set_header('Authorization', "Bearer #{token}")
+      render json: {
+        user: user.as_json(except: [:password_digest]), 
+        token: token
+      }, status: :created
       
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity 
     end
   end
 
   private
 
   def user_params
-    params.permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(
+      :username,
+      :email,
+      :height, 
+      :weight,
+      :gender,
+      :age,
+      :password, 
+      :password_confirmation)
   end
 end
